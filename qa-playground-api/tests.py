88de4106854800647770b182e43ddba00258bc8d
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2022-06-05 15:27:29 +0000

    Update tests.py

--- qa-playground-api/tests.py
+++ qa-playground-api/tests.py
@@ -1,30 +1,31 @@
 import requests
 import pytest
 
-def test_get_client():
-    response = requests.get("http://localhost:3000/")
-    assert response.status_code == 200
-    
-def test_create_user():
-    response = requests.post("http://localhost:3000/api/users/1", params={"email":"test@mail.ru", "first_name":"test", "last_name":"test"})
-    print(response.text)
-    assert response.status_code == 201
-    
-def test_put_user():
-    response = requests.put("http://localhost:3000/api/users/1", params={"email":"test1@mail.ru", "first_name":"test1", "last_name":"test1"})
-    print(response.text)
-    assert response.status_code == 201
-    
-def test_delete_user():
-    response = requests.delete("http://localhost:3000/api/users/1")
-    print(response.text)
-    assert response.status_code == 204
-    
-def test_check_list_users():
-    response = requests.get("http://localhost:3000/api/users/0")
-    print(response.text)
-    assert response.status_code == 200
-    
+class TestAPI:
+    """ Run tests: pytest -s -v tests.py """
+    def test_get_client():
+        response = requests.get("https://qaplaygroundapi.pythonanywhere.com/")
+        assert response.status_code == 200
+
+    def test_create_user():
+        response = requests.post("https://qaplaygroundapi.pythonanywhere.com/api/users/1", params={"email":"test@mail.ru", "first_name":"test", "last_name":"test"})
+        print(response.text)
+        assert response.status_code == 201
+
+    def test_put_user():
+        response = requests.put("https://qaplaygroundapi.pythonanywhere.com/api/users/1", params={"email":"test1@mail.ru", "first_name":"test1", "last_name":"test1"})
+        print(response.text)
+        assert response.status_code == 201
+
+    def test_delete_user():
+        response = requests.delete("https://qaplaygroundapi.pythonanywhere.com/api/users/1")
+        print(response.text)
+        assert response.status_code == 204
+
+    def test_check_list_users():
+        response = requests.get("https://qaplaygroundapi.pythonanywhere.com/api/users/0")
+        print(response.text)
+        assert response.status_code == 200
     
     
 
